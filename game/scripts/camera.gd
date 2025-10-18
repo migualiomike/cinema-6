e977f7e1c6f2cef895d41167301b6c7ef4ed3351
DrownedJester <Drownedjester@gmail.com>
2025-10-18 21:15:19 +1000

    Added RMB zoom

--- game/scripts/camera.gd
+++ game/scripts/camera.gd
@@ -11,11 +11,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("zoom"):
+		fov = lerpf(fov, 45.0, 5.0*delta)
+	elif !Input.is_action_pressed("zoom"):
+		fov = lerpf(fov, 75.0, 5.0*delta)
 
 func _input(event):  		
 	if event is InputEventMouseMotion:
 		rotation.x -= event.relative.y * mouseSensitivity
 		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
 		character_body_3d.rotation.y -= event.relative.x * mouseSensitivity
-		
