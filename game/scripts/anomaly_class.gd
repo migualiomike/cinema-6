f8df790185b1ed0d539e551e22b41ec9c44ba525
DrownedJester <Drownedjester@gmail.com>
2025-10-20 19:45:49 +1000

    Added formatting plugin

--- game/scripts/anomaly_class.gd
+++ game/scripts/anomaly_class.gd
@@ -1,5 +1,5 @@
-extends Node
 class_name Anomaly
+extends Node
 
 @export var chance := 1.0
 
@@ -11,8 +11,10 @@ var is_active := false:
 			on_deactivate()
 		is_active = value
 
+
 func on_activate() -> void:
 	pass
+
 
 func on_deactivate() -> void:
 	pass
--- game/scripts/level_manager_v2.gd
+++ game/scripts/level_manager_v2.gd
@@ -1,14 +1,11 @@
-extends Node
 class_name LevelManager
-
-@onready var player: CharacterBody3D = $"../CharacterBody3D"
-@onready var anomalies: Node = $Anomalies
-@onready var progress_number: Label3D = $ProgressNumber
+extends Node
 
 @export var tele_points: Array[Area3D] = []
 @export var debug := false
 
-var is_anomaly := false: set = is_anomaly_active
+var is_anomaly := false:
+	set = is_anomaly_active
 var anomaly_active: Node
 var anomaly_array: Array[Node] = []
 var anomaly_chances: Array[float] = []
@@ -16,15 +13,18 @@ var cur_progress := 0:
 	set(value):
 		progress_number.text = str(value)
 		cur_progress = value
-
 var random = RandomNumberGenerator.new()
+
+@onready var player: CharacterBody3D = $"../CharacterBody3D"
+@onready var anomalies: Node = $Anomalies
+@onready var progress_number: Label3D = $ProgressNumber
 
 
 func _ready() -> void:
 	for child in anomalies.get_children():
 		anomaly_array.append(child)
 		anomaly_chances.append(child.chance)
-	
+
 	for point in tele_points:
 		point.teleport.connect(on_loop)
 
@@ -32,15 +32,16 @@ func _ready() -> void:
 func on_loop(is_forward: bool) -> void:
 	on_progress(is_forward)
 	deactivate_current_anomaly()
-	
-	if randi() % 100 < (50 + (cur_progress * 2)):  # 50% chance and increases with each success
+
+	if randi() % 100 < (50 + (cur_progress * 2)): # 50% chance and increases with each success
 		is_anomaly = true
 	else:
 		is_anomaly = false
-	
+
 	if debug:
 		print("Is Anomaly: ", is_anomaly)
-		if anomaly_active: print("current anomaly: ", anomaly_active.name)
+		if anomaly_active:
+			print("current anomaly: ", anomaly_active.name)
 
 
 func on_progress(is_forward: bool) -> void:
@@ -50,24 +51,26 @@ func on_progress(is_forward: bool) -> void:
 				cur_progress = 0
 			false:
 				cur_progress += 1
-	
+
 	if not is_forward:
 		match is_anomaly:
 			true:
 				cur_progress += 1
 			false:
 				cur_progress = 0
-	
-	if debug: print("going_forward: ", is_forward)
-	if debug: print("current_progress: ", cur_progress)
-	if debug: print("current anomaly chance: ", 50 + (cur_progress * 2))
-	
+
+	if debug:
+		print("going_forward: ", is_forward)
+	if debug:
+		print("current_progress: ", cur_progress)
+	if debug:
+		print("current anomaly chance: ", 50 + (cur_progress * 2))
 
 
 func deactivate_current_anomaly() -> void:
 	if anomaly_active:
 		anomaly_active.is_active = false
-	
+
 	anomaly_active = null
 	is_anomaly = false
 
@@ -76,5 +79,5 @@ func is_anomaly_active(value: bool) -> void:
 	if value:
 		anomaly_active = anomaly_array[random.rand_weighted(anomaly_chances)]
 		anomaly_active.is_active = true
-	
+
 	is_anomaly = value
